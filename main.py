0cd668c302f34f208ac51d0ae13d20f51f7475da
Corey Maldonado <coreyam3@gmail.com>
2025-01-09 16:27:46 -0600

    Testing player addition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -9,10 +10,17 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player.draw(screen)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
         screen.fill("#000000", rect=None, special_flags=0)
         pygame.display.flip()
